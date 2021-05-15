4e33e1e77de1243395fdaf68bd6ca41d0fdef2f2
simonhochrein <simon.hochrein@icloud.com>
2021-05-15 08:55:01 -0400

    Initial Commit

---
+++ QR/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  QR
+//
+//  Created by Simon Hochrein on 5/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
